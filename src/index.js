cd609d3978bd25be1208f84c96070e3458db9c95
James Paterson <jamespaterson@Jamess-MacBook-Pro.local>
2022-01-30 18:07:17 -0500

    tweaks

--- src/index.js
+++ src/index.js
@@ -13,45 +13,30 @@ const {
 kickoffCJS()
 
 let range = 200
+
+// forming the general shape
 let circleContainer = new cjs.Container()
 container.addChild(circleContainer)
-
 let circles = _.times(3, makeCircle)
 _.map(circles, circle => {
-  circle.x = _.random(-range, range)
-  circle.y = _.random(-range, range)
+  placeItem(circle, 200)
   circleContainer.addChild(circle)
 })
 
+// only spawning assets based on general shape
 let dotContainer = new cjs.Container()
 container.addChild(dotContainer)
-
-// let dot = makeDot()
-// dot.x = _.random(-200, 200)
-// dot.y = _.random(-200, 200)
-// dotContainer.addChild(dot)
-
-// let pt = dot.localToLocal(0, 0, circleContainer)
-
-let dots = _.times(20, makeDot)
+let dots = _.times(100, makeDot)
 _.map(dots, dot => {
   dotContainer.addChild(dot)
   let pt
   do {
-    placeItem(dot, range)
+    placeItem(dot, 500)
     pt = dot.localToLocal(0, 0, circleContainer)
   } while (!circleContainer.hitTest(pt.x, pt.y))
 })
 
-function placeItem(item, range) {
-  item.x = _.random(-range, range)
-  item.y = _.random(-range, range)
-}
-
-
-// console.log("hit: ", circleContainer.hitTest(pt.x, pt.y))
-// console.log("pt: ", pt)
-
+////////////////
 ////////////////
 
 function kickoffCJS() {
@@ -61,6 +46,11 @@ function kickoffCJS() {
 
 function tick(e) {
   stage.update()
+}
+
+function placeItem(item, range) {
+  item.x = _.random(-range, range)
+  item.y = _.random(-range, range)
 }
 
 function makeCircle() {
